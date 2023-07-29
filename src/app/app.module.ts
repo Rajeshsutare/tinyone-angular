b17ee34e8252be54a8735b9ea2c8295b3be47be1
Rajeshsutare <sutarerns@gmail.com>
2023-07-29 13:59:54 +0530

    work on tinyone

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { InspirationComponent } from './shared/components/inspiration/inspiration.component';
+import { FeaturesComponent } from './shared/components/features/features.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavbarComponent,
+    InspirationComponent,
+    FeaturesComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
